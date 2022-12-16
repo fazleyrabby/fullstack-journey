0896e38d33624e75d979a95f4b0e6073f2211bc4
fazleyrabby <fazley111@gmail.com>
2022-12-17 02:23:38 +0600

    pagination link fix

--- react/src/components/Pagiantion.jsx
+++ react/src/components/Pagiantion.jsx
@@ -4,43 +4,31 @@ import axiosClient from '../axios-client.js';
 
 
 export default function Pagination({ links, callback }) {
-    function getClassName(active) {
-        return active ? "btn btn-sm active" : "btn btn-sm";
-    }
+    const getClassName = (active) => (active ? "btn btn-sm active" : "btn btn-sm")
 
-    function handleCallback(url){
+    const handleCallback = (url) => {
         axiosClient.get(url)
-        .then(({ data }) => {
-            callback(data)
-        })
+            .then(({ data }) => {
+                callback(data)
+            })
     }
 
     return (
         links && (
             <div>
-                <div style={{ display:"flex",gap:"5px" }}>
-                {/* {links.links.map((link, key) => (
-                        link.url === null ?
-                            (<div key={key}>{link.label}</div>) :
-
-                            (<Link
-                                className={getClassName(link.active)}
-                                href={link.url}
-                            >{link.label}</Link>)
-                    ))} */}
+                <div style={{ display: "flex", gap: "5px" }}>
                     {links.links && links.links.map((link, key) => (
                         link.url ? (
-                        <button key={key} className={getClassName(link.active)}
-                         style={{ cursor:"pointer" }} 
-                         onClick={e => handleCallback(link.url)}
-                         dangerouslySetInnerHTML={{__html: link.label}}>
-                        </button>) : (
                             <button key={key} className={getClassName(link.active)}
-                            dangerouslySetInnerHTML={{__html: link.label}}>
+                                style={{ cursor: "pointer" }}
+                                onClick={e => handleCallback(link.url)}
+                                dangerouslySetInnerHTML={{ __html: link.label }}>
+                            </button>) : (
+                            <button key={key} className="btn btn-sm disabled"
+                                dangerouslySetInnerHTML={{ __html: link.label }}>
                             </button>
                         )
                     ))
-
                     }
                 </div>
             </div>
