c1dc9c0e77f432a917823a8b8fdfee0ed32ea2cd
fazleyrabby <fazley111@gmail.com>
2022-12-26 07:37:17 +0600

    blog: crud fix with routing

--- react-node-mysql-blog/api/controllers/post.js
+++ react-node-mysql-blog/api/controllers/post.js
@@ -2,7 +2,28 @@ import { db } from "../db.js"
 import jwt from "jsonwebtoken"
 
 export const addPost = (request, response) => {
-    response.json("from controller add post!")
+    const token = request.cookies.access_token
+    if(!token) response.status(401).json("Not Authenticated!")
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if(err) response.status(403).json("Token is not valid!")
+
+        const query = "INSERT INTO posts(`title`,`desc`,`img`,`cat`,`date`,`uid`) VALUES (?)"
+
+        const values = [
+            request.body.title,
+            request.body.desc,
+            request.body.img,
+            request.body.cat,
+            request.body.date,
+            userInfo.id,
+        ]
+
+        db.query(query, [values], (err, data) => {
+            if(err) return response.status(500).json(err)
+            return response.json("Post created successfully!")
+        })        
+    })
 }
 
 export const getPosts = (request, response) => {
@@ -15,7 +36,7 @@ export const getPosts = (request, response) => {
 }
 
 export const getPost = (request, response) => {
-    const query = "SELECT p.id, `username`,`title`,`desc`, p.img, u.image AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id=?";
+    const query = "SELECT p.id,`username`,`title`,`desc`, p.img, u.image AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id=?";
 
     db.query(query, [request.params.id], (err, data) => {
         if(err) return response.status(500).json(err)
@@ -40,5 +61,24 @@ export const deletePost = (request, response) => {
 }
 
 export const updatePost = (request, response) => {
-    response.json("from controller update post!")
+    const token = request.cookies.access_token
+    if(!token) response.status(401).json("Not Authenticated!")
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if(err) response.status(403).json("Token is not valid!")
+        const postId = request.params.id
+        const query = "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id`=? AND `uid`=?"
+
+        const values = [
+            request.body.title,
+            request.body.desc,
+            request.body.img,
+            request.body.cat
+        ]
+
+        db.query(query, [...values, postId, userInfo.id], (err, data) => {
+            if(err) return response.status(500).json(err)
+            return response.json("Post updated successfully!")
+        })        
+    })
 }
