66c817e544d67ff43c9d8678aee643be2b74031b
fazleyrabby <fazley111@gmail.com>
2022-12-28 07:22:20 +0600

    social: layouts and theme function

--- react-node-mysql-social/client/src/App.js
+++ react-node-mysql-social/client/src/App.js
@@ -1,5 +1,6 @@
 import {
   createBrowserRouter,
+  Navigate,
   Outlet,
   RouterProvider,
 } from "react-router-dom";
@@ -11,27 +12,41 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
 
 function App() {
+  const currentUser = true;
+  const {darkMode} = useContext(DarkModeContext)
 
   const Layout = () => {
     return (
-      <div>
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
         <div style={{ display: "flex" }}>
           <Leftbar />
+          <div style={{ flex:6 }}>
           <Outlet />
+          </div>
+          
           <Rightbar />
         </div>
       </div>
     )
   }
 
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser){
+      return <Navigate to="/login/" />
+    }
+    return children
+  }
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: <ProtectedRoute><Layout /></ProtectedRoute>,
       children: [
         {
           path: "/",
--- react-node-mysql-social/client/src/components/navBar/Navbar.jsx
+++ react-node-mysql-social/client/src/components/navBar/Navbar.jsx
@@ -1,8 +1,44 @@
 import "./navbar.scss"
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Navbar = () => {
+  const { toggle, darkMode } = useContext(DarkModeContext)
+
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration:"none" }}>
+          <span>DummySocial</span>
+          {/* <img src="./logo.svg" alt="" width={130} /> */}
+        </Link>
+        <HomeOutlinedIcon />
+        {darkMode ? <WbSunnyOutlinedIcon onClick={toggle}/> : <DarkModeOutlinedIcon onClick={toggle}/> }
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon/>
+          <input type="text" name="search" id="search" placeholder="Search..."/>
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon/>
+        <EmailOutlinedIcon/>
+        <NotificationsOutlinedIcon/>
+        <div className="user">
+          <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+          <span>Demo User</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
