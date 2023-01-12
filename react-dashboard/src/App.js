2137a79f6ffce9dbb6d7d35065289d3b48969bc5
fazleyrabby <fazley111@gmail.com>
2023-01-12 07:26:23 +0600

    react-dashboard: init files

--- react-dashboard/src/App.js
+++ react-dashboard/src/App.js
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calender, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './index.css'
 
 const App = () => {
@@ -29,7 +30,6 @@ const App = () => {
                 </div>
 
                 <div>
-            
                     <Routes>
                         {/* Dashboard */}
                         <Route path='/' element="Ecommerce"/>
@@ -56,8 +56,6 @@ const App = () => {
                         <Route path='/pyramid' element="Pyramid"/>
                         <Route path='/stacked' element="Stacked"/>
                     </Routes>
-
-                    
                 </div>
             </div>
         </BrowserRouter>
