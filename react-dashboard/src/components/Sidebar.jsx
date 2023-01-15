6791ea4df1f4b52f3c71102b3713290242cf38af
fazleyrabby <fazley111@gmail.com>
2023-01-15 07:05:08 +0600

    react-dashboard: sidebar mobile fix

--- react-dashboard/src/components/Sidebar.jsx
+++ react-dashboard/src/components/Sidebar.jsx
@@ -7,7 +7,13 @@ import { links } from '../data/dummy'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-  const { activeMenu,setActiveMenu } = useStateContext();
+  const { activeMenu,setActiveMenu,screenSize } = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if(activeMenu && screenSize <= 900){
+      setActiveMenu(false)
+    }
+  }
   
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
 
@@ -17,7 +23,7 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to="/" onClick={() => setActiveMenu(false)} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+          <Link to="/" onClick={handleCloseSidebar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
             <SiShopware /> <span>Shoppyy</span>
           </Link>
           <TooltipComponent content="Menu" position="BottomCenter">
@@ -31,7 +37,7 @@ const Sidebar = () => {
           <div key={item.title}>
             <p className='text-gray-400 m-3 mt-4 uppercase'>{item.title}</p>
             {item.links.map((link) => (
-              <NavLink to={`/${link.name}`} key={link.name} onClick={() => {}} className={({isActive}) => isActive ? activeLink : normalLink}>
+              <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSidebar} className={({isActive}) => isActive ? activeLink : normalLink}>
                 {link.icon}
                 <span className='capitalize'>{link.name}</span>
               </NavLink>
