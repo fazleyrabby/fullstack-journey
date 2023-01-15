caece204b5aebbc0ec41c3c1103c2549d4c99790
fazleyrabby <fazley111@gmail.com>
2023-01-15 07:49:12 +0600

    react-dashboard: homepage cards

--- react-dashboard/src/components/Sidebar.jsx
+++ react-dashboard/src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
     }
   }
   
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 bg-slate-500'
 
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
 
