d5e48e842bfa40355a3da0a804fbfcde448592d4
fazleyrabby <fazley111@gmail.com>
2023-01-24 23:10:53 +0600

    airbnb: login page

--- airbnb-clone/client/src/pages/LoginPage.jsx
+++ airbnb-clone/client/src/pages/LoginPage.jsx
@@ -2,8 +2,15 @@ import Header from "../components/Header.jsx";
 
 export default function LoginPage(){
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="-mt-64">
+                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <form className="max-w-md mx-auto">
+                    <input type="email" placeholder='test@email.com'/>
+                    <input type="password" placeholder='Password'/>
+                    <button className='primary'>Login</button>
+                </form>
+            </div>
         </div>
     )
 }
