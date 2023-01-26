ffe2254a99a82fe55dca7bdd6a257d08fd56a0ea
fazleyrabby <fazley111@gmail.com>
2023-01-26 07:22:13 +0600

    airbnb: backend api user model, register with mongodb

--- airbnb-clone/api/index.js
+++ airbnb-clone/api/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs')
+const User = require('./models/User.js')
+require('dotenv').config()
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(express.json())
 
@@ -8,13 +14,26 @@ app.use(cors({
     credentials:true,
     origin: 'http://localhost:5173'
 }))
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL)
+
 app.get('/test', (req, res) => {
     res.json('testing!')
 })
 
-app.post('/register', (req, res) => {
+//mongo pass : bywPpw3ZFnGN6egl
+
+app.post('/register', async (req, res) => {
     const {name, email, password} = req.body;
-    res.json({name, email, password})
+
+    const userDoc = await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, salt),
+    })
+
+    res.json(userDoc)
 })
 
 app.listen(4000)
