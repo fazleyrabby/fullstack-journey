bab143c908e46a6edf1458843dca36d721d40ebf
fazleyrabby <fazley111@gmail.com>
2023-02-07 23:21:16 +0600

    airbnb: places update

--- airbnb-clone/client/src/App.jsx
+++ airbnb-clone/client/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
             <Route path="/login" element={<LoginPage/>} />
             <Route path="/register" element={<RegisterPage/>} />
             <Route path="/account/:subpage?" element={<AccountPage/>} />
+            <Route path="/account/:subpage/:action" element={<AccountPage/>} />
             {/*<Route path="/account/bookings" element={<AccountPage/>} />*/}
             {/*<Route path="/account/places" element={<AccountPage/>} />*/}
         </Route>
